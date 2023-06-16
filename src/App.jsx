48846af13b91714bdf510749b2f47a7b556b8eba
kartikhelps <kartik@techhelps.co.in>
2023-06-16 20:45:08 +0530

    added card and all

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
-
+import CustomCard from "./components/Card";
+import ComplexGrid from "./components/CardGrid";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { First } from "./First";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData(
+      import.meta.env.VITE_REACT_API_URL + "api/properties/list",
+      setData
+    );
+  }, []);
+
+  async function fetchData(url, setData) {
+    try {
+      const response = await axios.get(url);
+      setData(response.data.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  console.log(data);
+
   return (
     <>
-      
+      <ComplexGrid />
+      {/* <First/> */}
     </>
   );
 }
